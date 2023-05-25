47d0a2b0254856a1991dcb15a457b51de0033983
AndressaA <aandradeandressa@gmail.com>
2023-05-25 00:11:55 -0300

    atualizacao metodos

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,7 +4,6 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
-    const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = livros.filter(livro => livro.categoria == this.value)
     exibirOsLivrosNaTela(livrosFiltrados)
 }
